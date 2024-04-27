9920bc50d3404f4ac2cba7f32d42f13533efd3a6
Benjamin Weeg <Benjaminweeg@gmail.com>
2024-04-27 15:41:39 -0400

    getting started

--- api/inverted_index.py
+++ api/inverted_index.py
@@ -17,30 +17,17 @@ class AbstractInvertedIndex(ABC):
 			cls._instance = cls.__new__(cls)
 			cls._instance.documentPath = './documents' # Most likely overwritten by child class
 			cls._instance.indexer = defaultdict(list)
-
 			for property, value in kwargs.items():
 				setattr(cls._instance, property, value)
-
-<<<<<<< HEAD
 			cls._instance.loadDocuments()
-		
 		return cls._instance
-		
-	@abstractmethod
+
 	def loadDocuments(self):
-		pass
-=======
-            cls._instance.loadDocuments()
-        
-        return cls._instance
-        
-    def loadDocuments(self):
-        for dirPath, dirNames, files in os.walk(self.documentPath):
-            print(f'Found directory: {dirPath}')
-            for fileName in files:
-                fullPath = os.path.join(dirPath, fileName)
-                self.loadDocument(fullPath)    
->>>>>>> 9b477dd467f918fa1a1c07405595b57357c7dc0a
+		for dirPath, dirNames, files in os.walk(self.documentPath):
+			print(f'Found directory: {dirPath}')
+			for fileName in files:
+				fullPath = os.path.join(dirPath, fileName)
+				self.loadDocument(fullPath)	
 
 	@abstractmethod
 	def loadDocument(self, path):
@@ -49,6 +36,44 @@ class AbstractInvertedIndex(ABC):
 	@abstractmethod
 	def handleQuery(self, query: str) -> List[AbstractDocument]:
 		pass
+
+class BensInvertedIndex(AbstractInvertedIndex):
+	@classmethod
+	def getInstance(cls, documentPath='./documents'):
+		return super().getInstance(
+			documentPath=documentPath,
+			stopWords=stopwords.words('english')
+		)
+
+	def getTokens(self, data):
+		tokens = data.split()
+		filteredTokens = []
+		for token in tokens:
+			token = token.lower()
+			if not any(let.isdigit() for let in token) and token not in self.stopWords:
+				filteredTokens.append(token)
+		return filteredTokens
+
+	def _getNextId(self):
+		id = 1
+		while True:
+			yield id
+			id += 1
+	
+	def loadDocument(self, path):
+		document = SimpleDocument(path, id=self._getNextId())
+		with open(path, 'r') as file:
+			data = file.read()
+			tokens = self.getTokens(data)
+			for token in tokens:
+				self.indexer[token].append(document)
+
+	def handleQuery(self, query: str) -> List[AbstractDocument]:
+		queryWords = query.split()
+		postings = [self.indexer[queryWord] for queryWord in queryWords if queryWord not in self.stopWords]
+		commonDocuments = list(set.intersection(*map(set,postings))) if len(postings) > 0 else []
+		return commonDocuments
+#end of class BensInvertedIndex
 
 class SimpleInvertedIndex(AbstractInvertedIndex):
 	@classmethod
@@ -60,53 +85,27 @@ class SimpleInvertedIndex(AbstractInvertedIndex):
 
 	def getTokens(self, data):
 		tokens = data.split()
-
 		filteredTokens = []
 		for token in tokens:
 			token = token.lower()
 			if not any(let.isdigit() for let in token) and token not in self.stopWords:
 				filteredTokens.append(token)
-		
 		return filteredTokens
 
-<<<<<<< HEAD
 	def _getNextId(self):
 		id = 1
 		while True:
 			yield id
 			id += 1
 	
-	def loadDocuments(self):
-		for dirPath, dirNames, files in os.walk(self.documentPath):
-			print(f'Found directory: {dirPath}')
-			for fileName in files:
-				fullPath = os.path.join(dirPath, fileName)
-				self.loadDocument(fullPath)
-	
 	def loadDocument(self, path):
 		document = SimpleDocument(path, id=self._getNextId())
-		with open(path, 'r', encoding='utf-8') as file:
+		with open(path, 'r') as file:
 			data = file.read()
 			tokens = self.getTokens(data)
 			
 			for token in tokens:
 				self.indexer[token].append(document)
-=======
-    def _getNextId(self):
-        id = 1
-        while True:
-            yield id
-            id += 1
-    
-    def loadDocument(self, path):
-        document = SimpleDocument(path, id=self._getNextId())
-        with open(path, 'r') as file:
-            data = file.read()
-            tokens = self.getTokens(data)
-            
-            for token in tokens:
-                self.indexer[token].append(document)
->>>>>>> 9b477dd467f918fa1a1c07405595b57357c7dc0a
 
 	def handleQuery(self, query: str) -> List[AbstractDocument]:
 		queryWords = query.split()
@@ -114,8 +113,10 @@ class SimpleInvertedIndex(AbstractInvertedIndex):
 		commonDocuments = list(set.intersection(*map(set,postings))) if len(postings) > 0 else []
 		return commonDocuments
 
+
 def getInvertedIndex() -> AbstractInvertedIndex:
 	return SimpleInvertedIndex.getInstance()
+
 
 if __name__ == '__main__':
 	invertedIndex = SimpleInvertedIndex()
